9bcbae2bc82e98d0dbe9efce0397902834df1142
Mohannad Hisham <Mohannadize15@gmail.com>
2018-10-28 00:41:12 +0200

    Some Refactoring and style changes

---
+++ src/gallery.js
@@ -0,0 +1,8 @@
+const gallery = {
+    "The Starry Night": {
+        "link": "https://en.wikipedia.org/wiki/The_Starry_Night",
+        "artist": "Vincent van Gogh",
+        "layers":["base.png","top.png"]
+    }
+}
+export default gallery;
--- src/index.js
+++ src/index.js
@@ -2,30 +2,52 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Parallax from "parallax-js";
+import gallery from './gallery';
 
 function Intro() {
     return (<div>
         <h1>The Parallax Gallery Project</h1>
         <p>Welcome to the parallax Gallery Project.</p>
         <p>
-            This Project aims to extend the experience of art by intruducing a new 3d interactive dimention.
+            This Project aims to extend the experience of art by intruducing a new 3d interactive dimension.
             The Photos are edited using photoshop into depth layers then with the help of <a rel="noopener noreferrer" target="_blank" href="https://github.com/wagerfield/parallax">Parallax.js</a>, we can take benifit of these layers.
         </p>
     </div>)
 }
-class Content extends React.Component {
+let keys = Object.keys(gallery);
+class Gallery extends React.Component {
     render() {
-        return (<div id="scene" style={{overflow:"hidden",margin:"auto"}}>
-            <img data-depth="0.2" src="images/base.png" alt="failed to load"/>
-            <img data-depth="0.4" src="images/top.png" alt="failed to load"/>
-        </div>)
+        let counter = 0;
+        let haha = keys.map(name=>{
+            let artist = gallery[name].artist;
+            let dir = name.split(" ").join("");
+            let imgs = gallery[name].layers;
+            return (
+                <div>
+                    <h3>{name}</h3>
+                    <p>{artist}</p>
+                    <div className="parallax" key={dir} id={dir}>
+                        {imgs.map(x=>{
+                            counter++;
+                            return (<img alt="An error has occured"
+                                        key={counter.toString()}
+                                        data-depth={counter / 5}
+                                        src={"images/"+dir+"/"+x} />)
+                        })}
+                    </div>
+                    <a href={gallery[name].link} target="_blank" rel="noopener noreferrer" style={{display:"inline-block",margin:"10px"}}>Wiki</a>
+                </div>
+            )
+        });
+        return (haha)
     }
 }
 
 function Page() {
     return (<div>
         <Intro />
-        <Content />
+        <br/>
+        <Gallery />
     </div>)
 }
 // ========================================
@@ -34,9 +56,12 @@ ReactDOM.render(
     <Page />,
     document.getElementById('root')
 );
-const scene = document.getElementById("scene");
-// eslint-disable-next-line
-const parallaxinstance = new Parallax(scene,{
-    relativeInput:true,
-    hoverOnly:true
-});
+for (let name of keys) {
+    let dir = name.split(" ").join("");
+    const scene = document.getElementById(dir);
+    // eslint-disable-next-line
+    const parallaxinstance = new Parallax(scene,{
+        relativeInput:true,
+        hoverOnly:true
+    });    
+}
