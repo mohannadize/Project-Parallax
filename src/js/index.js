a49cdfc8672c3b204910c500b5acb56586cb269f
Mohannad Hisham <Mohannadize15@gmail.com>
2018-11-22 20:35:14 +0200

    Nearly finished everything

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,58 @@
 // Vibrant = require("node-vibrant");
 // import Vibrant from "node-vibrant";
 import gallery from "./gallery";
-
+import Parallax from "parallax-js";
 
 let arts = Object.keys(gallery);
 console.log(gallery)
 
+let elts = [];
 arts.map(name=>{
-    let counter = 0;
     let artist = gallery[name].artist;
     let dir = name;
     let imgs = gallery[name].layers;
     // let color = new Vibrant(`images/${dir}/${imgs[0]}`);
+    let gallerytag = document.getElementById("gallery");
+    let container = document.createElement("div");
 
+    let title = document.createElement("h3");
+    title.innerText = name.split("_").join(" ");
 
+    let desc = document.createElement("p");
+    desc.innerText = artist;
+
+    let parallaxCont = document.createElement("div")
+    parallaxCont.id = dir;
+
+    let counter = 0;
+    imgs = imgs.map(x=>{
+        let img = document.createElement("img");
+        img.alt = "Unable to load image";
+        img.dataset.depth = counter/5;
+        img.src = `images/${dir}/${x}`;
+        counter++;
+        return img;
+    })
+    imgs.map(x=>{
+        parallaxCont.append(x);
+    })
+
+    let a = document.createElement("a");
+    a.href = gallery[name].link;
+    a.target = "_blank";
+    a.rel = "noopener noreferrer";
+    a.innerText = "More info"
+
+    container.append(title,desc,parallaxCont,a);
+    gallerytag.append(container);
+
+    new Parallax(parallaxCont,{
+        relativeInput:true,
+        hoverOnly:true,
+        limitY: 4,
+        limitX: 10
+    });
 })
-
-
-// class Gallery extends React.Component {
-//     render() {
-//         let haha = keys.map((name)=>{
-//             let counter = 0;
-//             let artist = gallery[name].artist;
-//             let dir = name;
-//             let imgs = gallery[name].layers;
-//             let color = new Vibrant(`images/${dir}/${imgs[0]}`);
-//             color.getPalette().then(x=>{
-//                 let elem = document.querySelector("#"+dir);
-//                 elem.style.borderColor = x.DarkMuted ? x.DarkMuted.getHex() : "#222";
-//             });
 //             return (
 //                 <div key={dir+counter.toString()}>
 //                     <h3>{name.split("_").join(" ")}</h3>
@@ -74,3 +98,8 @@ arts.map(name=>{
 //         limitX: 10
 //     });    
 // }
+
+//             color.getPalette().then(x=>{
+//                 let elem = document.querySelector("#"+dir);
+//                 elem.style.borderColor = x.DarkMuted ? x.DarkMuted.getHex() : "#222";
+//             });
